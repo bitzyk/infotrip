8184669ca6fb58a8d99f8d0ecf1c672a78fa7bd3
Cristian Bitoi <cbitoi@radial.com>
2018-12-14 18:08:25 +0200

    autoupload

--- src/settings.php
+++ src/settings.php
@@ -11,7 +11,7 @@ define('ENV', $env);
 
 return [
     'settings' => [
-        'displayErrorDetails' => true, // set to false in production
+        'displayErrorDetails' => false, // set to false in production
         'addContentLengthHeader' => false, // Allow the web server to send the content-length header
 
         // Renderer settings
