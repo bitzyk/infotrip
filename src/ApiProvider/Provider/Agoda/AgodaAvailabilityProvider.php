92cc5ccb4b3f12e508f2a82f937b605b91c83348
Cristian Bitoi <cbitoi@radial.com>
2018-12-19 23:47:06 +0200

    composer pear/http_request2

--- src/ApiProvider/Provider/Agoda/AgodaAvailabilityProvider.php
+++ src/ApiProvider/Provider/Agoda/AgodaAvailabilityProvider.php
@@ -11,9 +11,39 @@ class AgodaAvailabilityProvider extends AbstractAvailabilityProvider
     implements IAvailabilityProvider
 {
 
+    const AUTHORIZATION = '1814466:daffcf5e-1a0e-45e5-b03a-8d68d8be67a3';
 
     protected function _checkAvailability()
     {
+        $request = new \HttpRequest();
+
+        $request
+            ->setHeaders([
+                'Authorization' => self::AUTHORIZATION,
+                'Content-Type' => 'application/json',
+            ]);
+
+        $request
+            ->setRawPostData(
+                '{
+"criteria": {
+"additional": { "currency": "USD", "discountOnly": false, "language": "en-us", "occupancy": {
+"numberOfAdult": 2,
+"numberOfChildren": 1 }
+},
+"checkInDate": "2018-12-25", "checkOutDate": "2018-12-27", "hotelId": [
+407854 ]
+} }'
+            );
+
+        $request->send();
+
+
+        var_dump($request->getResponseCode());
+        print_r($request->getResponseBody());
+
+        exit;
+
         $availabilityResponse = new AvailabilityResponse();
 
         $availabilityResponse->setCurrency('EUR');
